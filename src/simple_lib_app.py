d948ccbc16e62409df9e39ee96a6a27b849c0f86
Raphael Zulliger <zulliger@software-natives.ch>
2020-03-13 06:23:37 +0100

    Simplify system usage
    Use appropriate default values for members which usually do not need to be specified by the average user. In fact, many of these have been introduced to allow testing.

--- src/simple_lib_app.py
+++ src/simple_lib_app.py
@@ -1,15 +1,15 @@
 from cicd_sim import *
 
-output = StdOutput()
-bb = Repos()
-conan = Conan(StdOutput())
+# Setup environment
+github = Repos()
 artifactory = Artifactory()
-build_id = BuildIdGenerator()
-jenkins = Jenkins(artifactory, conan, bb, output, build_id)
+jenkins = Jenkins(artifactory, github)
 
-lib = bb.create_repo('lib', jenkins)
-app = bb.create_repo('app', jenkins)
+# Create Git repos/projects
+lib = github.create_repo('lib', jenkins)
+app = github.create_repo('app', jenkins)
 
+# Start developing
 lib_dev = lib.create_branch('develop', 'blue')
 lib_dev.commit_file('VERSION', '1.0.1')
 lib_dev.push()
@@ -18,5 +18,3 @@ app_dev = app.create_branch('develop', 'blue')
 app_dev.commit_file('VERSION', '1.0.0')
 app_dev.commit_file('REQUIRES', 'lib/1.x')
 app_dev.push()
-
-lib_dev.push()
